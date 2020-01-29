35902689b9097c59f948d95f8d58a427b6ed8d56
Franco <francoetche@gmail.com>
2020-01-29 20:22:52 -0300

    Explorar: pantalla inicial

--- app/src/main/java/ar/uba/fi/remy/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/ar/uba/fi/remy/ui/dashboard/DashboardFragment.kt
@@ -12,20 +12,17 @@ import ar.uba.fi.remy.R
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        //Título de la pantalla
+        val textView: TextView = root.findViewById(R.id.dashboard_title)
+        textView.text = getString(R.string.dashboard_title)
+
         return root
     }
 }
