48ee94d6fd1d18046c5d25157634564343164557
Franco <francoetche@gmail.com>
2020-02-25 00:33:41 -0300

    ClickListener a Cardview Inventario

--- app/src/main/java/ar/uba/fi/remy/ui/perfil/PerfilFragment.kt
+++ app/src/main/java/ar/uba/fi/remy/ui/perfil/PerfilFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import ar.uba.fi.remy.R
 
 class PerfilFragment : Fragment() {
@@ -19,6 +18,15 @@ class PerfilFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_perfil, container, false)
 
+        val cardInventario: CardView = root.findViewById(R.id.card_inventario)
+        cardInventario.setOnClickListener{
+            goInventario()
+        }
+
         return root
+    }
+
+    private fun goInventario() {
+        Toast.makeText(activity, "Click Inventario", Toast.LENGTH_LONG).show()
     }
 }
