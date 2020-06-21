1f7cc25d272197fc71df1ec36a7db75b25c1e538
Franverri <francoetche@gmail.com>
2020-06-21 14:06:14 -0300

    Scanner inicial

--- app/src/main/java/ar/uba/fi/remy/ui/inventory/InventoryFragment.kt
+++ app/src/main/java/ar/uba/fi/remy/ui/inventory/InventoryFragment.kt
@@ -1,18 +1,27 @@
 package ar.uba.fi.remy.ui.inventory
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import ar.uba.fi.remy.R
+import ar.uba.fi.remy.ScannerActivity
 import ar.uba.fi.remy.model.InventoryAdapter
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.fragment_inventory.*
 import kotlinx.android.synthetic.main.fragment_inventory.view.*
 import org.json.JSONArray
 import org.json.JSONObject
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class InventoryFragment : Fragment() {
 
@@ -55,7 +64,35 @@ class InventoryFragment : Fragment() {
 
         })
 
+        root.inventory_floating.setOnClickListener(View.OnClickListener {
+            goScanner()
+        })
+
         return root
+    }
+
+    private fun goScanner() {
+        /*val intent = Intent(activity, ScannerActivity::class.java)
+        startActivity(intent)*/
+        Log.i("API", "Entra aca 1")
+        val scanner = IntentIntegrator(activity)
+        scanner.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.i("API", "Entra aca 2")
+        if(resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(activity, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(activity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 
     private fun cargarInventario(inventario: String, inventoryList: ListView) {
