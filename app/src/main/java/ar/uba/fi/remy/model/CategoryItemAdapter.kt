f50006523aabbe5fcca98935808f3ef5a2f94119
Franverri <francoetche@gmail.com>
2020-07-11 14:24:57 -0300

    Adapter for categories simulating API request

---
+++ app/src/main/java/ar/uba/fi/remy/model/CategoryItemAdapter.kt
@@ -0,0 +1,31 @@
+package ar.uba.fi.remy.model
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ar.uba.fi.remy.R
+
+class CategoryItemAdapter(var listaCategorias:Array<String>): RecyclerView.Adapter<CategoryItemAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.category_item, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return listaCategorias.size
+    }
+
+    override fun onBindViewHolder(holder:CategoryItemAdapter.ViewHolder, position: Int) {
+        holder.bindItems(listaCategorias[position])
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        fun bindItems(data: String){
+
+        }
+
+    }
+
+}
--- app/src/main/java/ar/uba/fi/remy/ui/explorar/ExplorarFragment.kt
+++ app/src/main/java/ar/uba/fi/remy/ui/explorar/ExplorarFragment.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ar.uba.fi.remy.R
+import ar.uba.fi.remy.model.CategoryItemAdapter
 
 class ExplorarFragment : Fragment() {
 
@@ -18,9 +22,24 @@ class ExplorarFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_explorar, container, false)
 
         //Título de la pantalla
-        val textView: TextView = root.findViewById(R.id.dashboard_title)
-        textView.text = getString(R.string.dashboard_title)
+        /*val textView: TextView = root.findViewById(R.id.dashboard_title)
+        textView.text = getString(R.string.dashboard_title)*/
+
+        //Obtengo las categorias de la API
+        cargarCategorias(root)
 
         return root
+    }
+
+    private fun cargarCategorias(root: View) {
+        val recyclerView: RecyclerView = root.findViewById(R.id.rv_categorias)
+        recyclerView.layoutManager = GridLayoutManager(activity, 4)
+
+        //Reemplazar por el llamado a la API para obtener las recetas
+        val categorias = arrayOf("Categoria 1", "Categoria 2", "Categoria 3", "Categoria 4", "Categoria 5")
+
+
+        val adapter = CategoryItemAdapter(categorias)
+        recyclerView.adapter = adapter
     }
 }
