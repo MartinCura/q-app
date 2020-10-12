3a918d73073dddc5258a9308aabf7ec20696da65
Franverri <francoetche@gmail.com>
2020-10-12 14:11:12 -0300

    Basic dialog for friend list getting data from API (Missing showing that data)

--- app/src/main/java/ar/uba/fi/remy/DetailEventActivity.kt
+++ app/src/main/java/ar/uba/fi/remy/DetailEventActivity.kt
@@ -1,5 +1,6 @@
 package ar.uba.fi.remy
 
+import android.app.Dialog
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,11 +8,10 @@ import android.util.Log
 import android.view.View
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_detail_event.*
-import org.json.JSONArray
-import org.json.JSONObject
 
 class DetailEventActivity : AppCompatActivity() {
     lateinit var token: String
@@ -29,11 +29,45 @@ class DetailEventActivity : AppCompatActivity() {
         Log.i("API", "ID Evento: " + idEvento)
 
         event_add_person_btn.setOnClickListener(View.OnClickListener {
-          addPerson(idEvento)
+            //addPerson(idEvento, 0)
+            //showDialog()
+            loadFriends()
         })
     }
 
-    private fun addPerson(idEvento: Int) {
+    private fun loadFriends() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://tpp-remy.herokuapp.com/api/v1/profiles/friends/"
+
+        val jsonArrayRequest = object: JsonArrayRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Log.i("API", "Response: $response")
+                showDialog()
+            },
+            Response.ErrorListener { error ->
+                Log.e("API", "Error en GET")
+                Log.e("API", "Response: %s".format(error.toString()))
+            }
+        )
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "Token " + token
+                return headers
+            }
+        }
+
+        queue.add(jsonArrayRequest)
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen)
+        dialog.setContentView(R.layout.dialog_list_friends)
+        dialog.show()
+    }
+
+    private fun addPerson(idEvento: Int, idFriend: Int) {
         val queue = Volley.newRequestQueue(this)
         //TO-DO: Agregar Dialog o algo para seleccionar al invitado a agregar
         val url = "https://tpp-remy.herokuapp.com/api/v1/events/" + idEvento + "/add_attendee/?attendee_id=" + "10"
