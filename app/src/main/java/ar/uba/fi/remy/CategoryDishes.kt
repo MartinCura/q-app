e2552ca612549d4197248698c07302fc64d79972
Franverri <francoetche@gmail.com>
2020-10-12 11:01:30 -0300

    Get dishes from a certain cagegory

--- app/src/main/java/ar/uba/fi/remy/CategoryDishes.kt
+++ app/src/main/java/ar/uba/fi/remy/CategoryDishes.kt
@@ -1,14 +1,53 @@
 package ar.uba.fi.remy
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class CategoryDishes : AppCompatActivity() {
+
+    lateinit var token: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_dishes)
         Log.i("API", intent.getIntExtra("idCategory", 0).toString())
+
+        //Obtener token
+        val sharedPref = this.getSharedPreferences(
+            getString(R.string.preference_file), Context.MODE_PRIVATE)
+        token = sharedPref?.getString("TOKEN", "")!!
+
+        loadDoshes(intent.getIntExtra("idCategory", 0))
+    }
+
+    private fun loadDoshes(id: Int) {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://tpp-remy.herokuapp.com/api/v1/dishes/?labels=" + id
+
+        val jsonObjectRequest = object: JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Log.i("API", response.toString())
+            },
+            Response.ErrorListener { error ->
+                Log.e("API", "Error en GET")
+                Log.e("API", "Response: %s".format(error.toString()))
+            }
+        )
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "Token " + token
+                return headers
+            }
+        }
+
+        queue.add(jsonObjectRequest)
     }
 }
