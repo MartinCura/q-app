30a78e93c025087b1a0613be31cab9e2e6f29172
Franverri <francoetche@gmail.com>
2020-11-10 17:05:53 -0300

    Create event from screen + Missing reload events list

--- app/src/main/java/ar/uba/fi/remy/NewEventActivity.kt
+++ app/src/main/java/ar/uba/fi/remy/NewEventActivity.kt
@@ -1,21 +1,34 @@
 package ar.uba.fi.remy
 
 import android.app.DatePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import ar.uba.fi.remy.model.DatePickerFragment
 import ar.uba.fi.remy.model.TimePicker
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_new_event.*
+import org.json.JSONArray
+import org.json.JSONObject
 import java.util.*
 
 class NewEventActivity : AppCompatActivity() {
 
+    lateinit var token: String
     lateinit var timePicker: TimePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_event)
+
+        //Obtener token
+        val sharedPref = this?.getSharedPreferences(
+            getString(R.string.preference_file), Context.MODE_PRIVATE)
+        token = sharedPref?.getString("TOKEN", "")!!
 
         new_event_date.setOnClickListener {
             showDatePickerDialog()
@@ -48,11 +61,47 @@ class NewEventActivity : AppCompatActivity() {
 
     private fun createEvent() {
         if(validarCampos()) {
+            var name = new_event_title.text.toString()
             var date = new_event_date.text.toString()
-            date.replace(" / ", "-")
+            var hour = new_event_time.text.toString()
+            var splitDate = date.split(" / ");
+            var formattedDate = ""
+            formattedDate = splitDate[2] + "-" + splitDate[1] + "-" + splitDate[0]
+            var finalDate = formattedDate + "T" + hour +":00"
+            Log.i("API", finalDate)
 
-            Log.i("API", date)
-            Log.i("API", new_event_title.text.toString())
+            val body = JSONObject()
+            body.put("attendees_id",JSONArray())
+            body.put("name", name)
+            body.put("starting_datetime", finalDate)
+            body.put("finishing_datetime", finalDate)
+            body.put("only_host_inventory", true)
+
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://tpp-remy.herokuapp.com/api/v1/events/"
+
+            Log.i("API", body.toString())
+
+            val jsonObjectRequest = object: JsonObjectRequest(
+                Request.Method.POST, url, body,
+                Response.Listener { response ->
+                    Log.i("API", "Response: %s".format(response.toString()))
+                    finish()
+                },
+                Response.ErrorListener { error ->
+                    Log.e("API", "Error en POST")
+                    Log.e("API", "Response: %s".format(error.toString()))
+                }
+            )
+            {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Authorization"] = "Token " + token
+                    return headers
+                }
+            }
+
+            queue.add(jsonObjectRequest)
         }
     }
 
@@ -76,6 +125,12 @@ class NewEventActivity : AppCompatActivity() {
         if(new_event_date.text.isNullOrEmpty()){
             new_event_date.error = "Este campo es obligatorio"
             new_event_date.requestFocus()
+            return false
+        }
+
+        if(new_event_time.text.isNullOrEmpty()){
+            new_event_time.error = "Este campo es obligatorio"
+            new_event_time.requestFocus()
             return false
         }
 
