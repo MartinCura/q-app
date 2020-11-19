51dcfe0c9508983d655287904683d1fcb2367449
Franverri <francoetche@gmail.com>
2020-11-19 15:00:32 -0300

    Add search view to new contact screen

--- app/src/main/java/ar/uba/fi/remy/NewContactActivity.kt
+++ app/src/main/java/ar/uba/fi/remy/NewContactActivity.kt
@@ -2,11 +2,33 @@ package ar.uba.fi.remy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.SearchView
+import kotlinx.android.synthetic.main.activity_new_contact.*
 
 class NewContactActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_contact)
+
+        configSearch()
+    }
+
+    private fun configSearch() {
+        new_contact_search.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    Log.i("API", newText)
+                }
+                return true
+            }
+
+        })
     }
 }
