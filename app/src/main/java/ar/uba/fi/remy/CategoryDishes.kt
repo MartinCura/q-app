fa7cf0d6c162681ef3cbe9c971a24596a22521a9
Franverri <francoetche@gmail.com>
2020-12-23 11:06:26 -0300

    Show explore options in each category selected

--- app/src/main/java/ar/uba/fi/remy/CategoryDishes.kt
+++ app/src/main/java/ar/uba/fi/remy/CategoryDishes.kt
@@ -4,14 +4,24 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ar.uba.fi.remy.model.RecommendedItem
+import ar.uba.fi.remy.model.RecommendedItemAdapter
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import java.lang.Math.round
 
 class CategoryDishes : AppCompatActivity() {
 
     lateinit var token: String
+    var urlNext:String = ""
+    var loadingMore = false
+    val recomendaciones = ArrayList<RecommendedItem>()
+    lateinit var rvAdapter: RecommendedItemAdapter
+    var id = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +33,55 @@ class CategoryDishes : AppCompatActivity() {
             getString(R.string.preference_file), Context.MODE_PRIVATE)
         token = sharedPref?.getString("TOKEN", "")!!
 
-        loadDoshes(intent.getIntExtra("idCategory", 0))
+        id =  intent.getIntExtra("idCategory", 0)
+        setupRecycler()
+        loadDishes()
     }
 
-    private fun loadDoshes(id: Int) {
+    private fun setupRecycler() {
+        val recyclerView: RecyclerView = this.findViewById(R.id.rv_explore)
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        rvAdapter = RecommendedItemAdapter(recomendaciones)
+        recyclerView.adapter = rvAdapter
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    if(!loadingMore && urlNext != "null") {
+                        loadDishes()
+                    }
+                }
+            }
+        })
+    }
+
+    private fun loadDishes() {
+        loadingMore = true
         val queue = Volley.newRequestQueue(this)
-        val url = "https://tpp-remy.herokuapp.com/api/v1/dishes/?labels=" + id
+        var url = urlNext
+        if(url.isBlank()) {
+            url = "https://tpp-remy.herokuapp.com/api/v1/dishes/?labels=" + id
+        }
 
         val jsonObjectRequest = object: JsonObjectRequest(
             Request.Method.GET, url, null,
             Response.Listener { response ->
                 Log.i("API", response.toString())
+                var recomendacionesArray = response.getJSONArray("results")
+                loadingMore = false
+                urlNext = response.getString("next")
+                for (i in 0 until recomendacionesArray.length()) {
+                    var recomendacion = recomendacionesArray.getJSONObject(i)
+                    var title = recomendacion.getString("name")
+                    var id = recomendacion.getInt("id")
+                    /*var score = round(recomendacion.getString("score").toDouble() / 2)*/
+                    var score = 3
+                    recomendaciones.add(RecommendedItem(id, title, score.toInt(), 15,  20, 1))
+                    rvAdapter.notifyDataSetChanged()
+                }
             },
             Response.ErrorListener { error ->
                 Log.e("API", "Error en GET")
