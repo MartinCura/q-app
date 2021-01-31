0b6380abb67131f89419509eb92fca21fd740750
Franverri <francoetche@gmail.com>
2021-01-31 13:50:27 -0300

    Add navigation drawer initial

--- app/src/main/java/ar/uba/fi/remy/MainActivity.kt
+++ app/src/main/java/ar/uba/fi/remy/MainActivity.kt
@@ -1,19 +1,75 @@
 package ar.uba.fi.remy
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import ar.uba.fi.remy.model.ClickListener
+import ar.uba.fi.remy.model.NavigationItemModel
+import ar.uba.fi.remy.model.NavigationRVAdapter
+import ar.uba.fi.remy.model.RecyclerTouchListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    private lateinit var adapter: NavigationRVAdapter
+    private var items = arrayListOf(
+        NavigationItemModel(R.drawable.ic_home, "Home"),
+        NavigationItemModel(R.drawable.ic_shopping_cart, "Carrito")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.supportActionBar?.hide()
+        /*this.supportActionBar?.hide()*/
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        // Side Navigation Menu
+        // Set the toolbar
+        setSupportActionBar(toolbar)
+
+        // Setup Recyclerview's Layout
+        navigation_rv.layoutManager = LinearLayoutManager(this)
+        navigation_rv.setHasFixedSize(true)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Icon click
+        toolbar_icon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Add Item Touch Listener
+        navigation_rv.addOnItemTouchListener(RecyclerTouchListener(this, object : ClickListener {
+            override fun onClick(view: View, position: Int) {
+                when (position) {
+                    0 -> {
+                        // # Home Fragment
+                        Log.i("API", "Home")
+                    }
+                    1 -> {
+                        // # Changuito Fragment
+                        Log.i("API", "Changuito")
+                    }
+                }
+                Handler().postDelayed({
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }, 200)
+            }
+        }))
+
+        updateAdapter(0)
+
+        // Bottom Navigation Menu
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -27,5 +83,27 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun updateAdapter(highlightItemPos: Int) {
+        adapter = NavigationRVAdapter(items, highlightItemPos)
+        navigation_rv.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            // Checking for fragment count on back stack
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                // Go to the previous fragment
+                supportFragmentManager.popBackStack()
+            } else {
+                // Exit the app
+                super.onBackPressed()
+            }
+        }
     }
 }
---
+++ app/src/main/java/ar/uba/fi/remy/model/NavigationItemModel.kt
@@ -0,0 +1,3 @@
+package ar.uba.fi.remy.model
+
+data class NavigationItemModel(var icon: Int, var title: String)
